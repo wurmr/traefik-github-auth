e9da8ee1cca90abe5014e5806884bce466a422a4
Jim Karg <wurmr@users.noreply.github.com>
2022-12-21 22:14:36 +0000

    Adjust middleware to auto login

--- src/Middleware/auth.js
+++ src/Middleware/auth.js
@@ -1,11 +1,12 @@
 const forceLogin = (req, res, next) => {
   const ALLOWED_USERS = process.env.ALLOWED_USERS
 
-  if (ALLOWED_USERS.split(',').includes(req.user?.username)) {
+  if (!req.user) {
+    res.redirect('/login')
+  } else if (ALLOWED_USERS.split(',').includes(req.user?.username)) {
     next()
   } else {
     res.status(401).send('KO')
-    // res.redirect('/login')
   }
 }
 
